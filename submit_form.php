83e7cf0eae3bb20b783852b6e4c1cb7fb1a9c2d6
soaad-Ch <134018567+https-soaad@users.noreply.github.com>
2024-11-11 19:34:45 +0100

    Update submit_form.php

--- submit_form.php
+++ submit_form.php
@@ -47,8 +47,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $mail->isSMTP();
         $mail->Host       = 'smtp.gmail.com';
         $mail->SMTPAuth   = true;
-        $mail->Username   = 'devlopwfs@gmail.com'; // Votre adresse Gmail
-        $mail->Password   = 'dbdh mepf hagr nmja'; // Mot de passe Gmail ou App Password
+        $mail->Username   = 'user@gmail.com.com'; // Votre adresse Gmail
+        $mail->Password   = 'xxx xxx xxx xxx'; // Mot de passe Gmail ou App Password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
         $mail->Port       = 587;
 
